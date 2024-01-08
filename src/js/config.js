23dd1a527c427e44a9ff3b69492ee4c8476e148e
peter nady <nady.peter347@gmail.com>
2024-01-08 10:13:46 +0200

    changing the api key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 13;
-export const KEY = "a25783ad-71ea-42b2-aba8-6911ebccf243";
+export const KEY = "bcbeffaf-eb15-4654-987f-cb0b6f368d67";
 export const MODAL_CLOSE_SEC = 2.5;
